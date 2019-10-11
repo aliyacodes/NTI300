41405885a6c337afe3917e940850c4112894af75
Aliya <22922673+aliyacodes@users.noreply.github.com>
2019-10-10 18:47:19 -0700

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+if [ -e /usr/bin/wget ]; then
+   exit 0;
+ fi
 yum -y install wget
 https://raw.githubusercontent.com/aliyacodes/NTI300/master/packages.txt
 for packages in $(cat packages.txt); do 
