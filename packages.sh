39770e7fa334a2d62ee90d3be02799009e9fb424
Aliya <22922673+aliyacodes@users.noreply.github.com>
2019-10-10 19:14:34 -0700

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 if [ -e /usr/bin/wget ]; then
    exit 0;
- fi
+fi
 yum -y install wget
-https://raw.githubusercontent.com/aliyacodes/NTI300/master/packages.txt
+wget https://raw.githubusercontent.com/aliyacodes/NTI300/master/packages.txt
 for packages in $(cat packages.txt); do 
    yum -y install $packages
 done
