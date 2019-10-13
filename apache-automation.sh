14f4d7b5e8afc1ec090850a0ca1266532b8c83a3
Aliya <22922673+aliyacodes@users.noreply.github.com>
2019-10-13 14:08:48 -0700

    Update apache-automation.sh

--- apache-automation.sh
+++ apache-automation.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+if [ -e /etc/httpd ]; then
+  exit 0                   
+fi   
 yum -y install httpd mod_ssl                                                              # Install apache and SSL support
 systemctl start httpd                                                                     # Start apache
 sed -i 's/^/#/g' /etc/httpd/conf.d/welcome.conf                                           # Comment out the default welcome page
