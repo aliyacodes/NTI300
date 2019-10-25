795e290c353ffcad9fcfafcf0d3d0f0a48e676b5
Aliya <22922673+aliyacodes@users.noreply.github.com>
2019-10-24 19:48:03 -0700

    Update django.sh

--- django.sh
+++ django.sh
@@ -11,7 +11,8 @@ pip install django
 django-admin --version    
 django-admin startproject newproject   
 cd newproject/     
-python manage.py migrate   
-python manage.py createsuperuser  # <-- work around this
-newproject/settings.py            # <-- search and replace
+python manage.py migrate
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@newproject.com','NTI300NTI300')" | python manage.py shell   
+myip=$(curl -s checkip.dyndns.org | sed -e 's/.*Current IP Address: //' -e 's/<.*$//')
+sed -i "s/ALLOWED_HOSTS = \[\]/ALLOWED_HOSTS = \[\'$myip\'\]/g" newproject/settings.py 
 python manage.py runserver 0.0.0.0:8000  
