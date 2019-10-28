b37fc6aa09e1e6a166557b96917587e22607f926
Aliya <22922673+aliyacodes@users.noreply.github.com>
2019-10-27 20:40:03 -0700

    Update test_status2.sh

--- test_status2.sh
+++ test_status2.sh
@@ -1,47 +1,46 @@
 #!/bin/bash
 
-#if nothing entered, prompt for argument
-if [ -z "$1" ] ; then
-    echo "Please specify the service or package you would like to install."
+if [ -z $1 ]
+then
+        echo "Please specify the program or service you would like to install. "
+        exit 0;
+
 fi
 
-# Test to see if the package/service being queried about is actually installed.  
-rpm -q $1
-status=$(systemctl status $1 | grep Installed | awk '{print $2}')
-uninstalled='uninstalled'
+status=$(rpm -q $1 | grep Installed | awk '{print $2}')
+installed=$(rpm -q $1 | grep $1 | awk '{print $2}')
 
-# If not, prompt the user to see if they would like it installed, then do so if they would 
-# or exit 0 if they would not.
-while true; do
-    read -p "Would you like to install this program? [y/n]" yn
-    case $yn in
-        [Yy]* ) sudo yum -y install $1; break;;
-        [Nn]* ) exit 0;;
-        * ) echo "Please answer yes or no.";;
-    esac
-done
+if [[ $status != $installed ]]
+then
 
-# If the service is off, prompt the user to see if they would like it turned on.  
-# If they do want it turned on, start the service, 
-# if not, exit 0.
-if [ -z "$1" ] ; then
-    echo "Please specify the service or package you would like turned on"
+        echo "--> ${1} is not installed."
+        read -p "--> Would you like to install ${1}? [y/n] " yn
+                case $yn in
+                        [Yy]* ) yum -y install $1;;
+                        [Nn]* ) echo "### Exiting installation program."; exit 0;;
+                        * ) echo "--> Please answer yes or no.";; #fix
+                esac
+        read -p "--> Would you like to start ${1}? [y/n] " yn
+                case $yn in
+                        [Yy]* ) systemctl start $1;;
+                        [Nn]* ) echo "### Exiting activation program."; exit 0;;
+                        * ) echo "--> Please answer yes or no.";; #fix
+                esac
+else
+        echo "--> ${1} is installed"
+        status=$(systemctl status $1 | grep Active | awk '{print $2}')
+        inactive='inactive'
+                if [[ $status == $inactive ]]
+                then
+                        read -p "--> Would you like to start ${1}? [y/n] " yn
+                                case $yn in
+                                        [Yy]* ) systemctl start $1;;
+                                        [Nn]* ) echo "### Exiting activation program."; exit 0;;
+                                        * ) echo "--> Please answer yes or no.";; #fix
+                                esac
+                        echo "--> ${1} is now ${status}."
+                else
+                        echo "--> ${1} is ${status}."
+                fi
+
 fi
-
-# Test to see if the package/service being queried about is active. 
-rpm -q $1
-status=$(systemctl status $1 | grep Active | awk '{print $2}')
-inactive='inactive'
-
-if [[ $status == $inactive ]]; then
-read -p "Would you like to turn on this service? [y/n]" yn
-    case $yn in
-            [Yy]* ) sudo systemctl start $1;;
-            [Nn]* ) exit 0;;
-            * ) echo "Please answer yes or no.";;
-    esac
-fi
-
-echo "-----------------------------------------------------------------"
-sudo ps aux | grep $1
-
